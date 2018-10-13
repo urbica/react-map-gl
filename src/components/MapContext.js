0cdf0243b6e5211caeb61804f55a090085f64feb
boyur <gboyur@gmail.com>
2018-10-13 17:51:08 +0300

    context fixed

--- src/components/MapContext.js
+++ src/components/MapContext.js
@@ -1,10 +1,11 @@
 // @flow
 import React from 'react';
 import type { Context } from 'react';
-import createReactContext from 'create-react-context';
+import createReactContext, { type Context as ContextPolyfill } from 'create-react-context';
 
 const reactVersion = parseInt(React.version, 10);
-const MapContext: Context<?MapboxMap> =
-  reactVersion < 16 ? createReactContext(null) : React.createContext(null);
 
-export default MapContext;
+const MapContext: Context<?MapboxMap> = React.createContext && React.createContext(null);
+const MapContextPolyfill: ContextPolyfill<?MapboxMap> = createReactContext(null);
+
+export default (reactVersion < 16 ? MapContextPolyfill : MapContext);
