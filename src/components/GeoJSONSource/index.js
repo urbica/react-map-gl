0fe9f631302c26f770de6089a217e55ac1348c65
Stepan Kuzmin <to.stepan.kuzmin@gmail.com>
2019-02-19 16:51:24 +0300

    feat(GeoJSONSource): add GeoJSONSource

---
+++ src/components/GeoJSONSource/index.js
@@ -0,0 +1,68 @@
+// @flow
+
+import { PureComponent, createElement } from 'react';
+import type MapboxMap from 'mapbox-gl/src/ui/map';
+import type {
+  StyleSpecification,
+  GeoJSONSourceSpecification
+} from 'mapbox-gl/src/style-spec/types';
+
+import MapContext from '../MapContext';
+
+type Props = GeoJSONSourceSpecification & {
+  id: string
+};
+
+class GeoJSONSource extends PureComponent<Props> {
+  _map: MapboxMap;
+
+  static displayName = 'GeoJSONSource';
+
+  componentDidMount() {
+    const { id, ...source } = this.props;
+    this._map.addSource(id, source);
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    const { id, ...source } = this.props;
+
+    if (id !== prevProps.id) {
+      this._map.removeSource(prevProps.id);
+      this._map.addSource(id, source);
+      return;
+    }
+
+    if (source.data !== prevProps.data) {
+      this._map.getSource(id).setData(source.data);
+    }
+  }
+
+  componentWillUnmount() {
+    if (!this._map || !this._map.getStyle()) {
+      return;
+    }
+
+    const { id } = this.props;
+    if (this._map.getSource(id)) {
+      const style: StyleSpecification = this._map.getStyle();
+      if (style.layers) {
+        style.layers
+          .filter(layer => layer.source && layer.source === id)
+          .forEach(layer => this._map.removeLayer(layer.id));
+      }
+
+      this._map.removeSource(id);
+    }
+  }
+
+  render() {
+    return createElement(MapContext.Consumer, {}, (map: ?MapboxMap) => {
+      if (map) {
+        this._map = map;
+      }
+      return null;
+    });
+  }
+}
+
+export default GeoJSONSource;
