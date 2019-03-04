2f143d02868d4ae31c52baa21d4a0222652b1094
Stepan Kuzmin <to.stepan.kuzmin@gmail.com>
2019-03-04 18:22:00 +0300

    test: cleanup

--- src/components/MapGL/index.test.js
+++ src/components/MapGL/index.test.js
@@ -1,28 +1,32 @@
 import React from 'react';
 import { mount } from 'enzyme';
+
 import MapGL from '../..';
 
 test('MapGL#render', () => {
   const wrapper = mount(<MapGL latitude={0} longitude={0} zoom={0} />);
+  const map = wrapper.instance().getMap();
+
   expect(wrapper.exists()).toBe(true);
 
   wrapper.unmount();
   expect(wrapper.exists()).toBe(false);
+  expect(map.remove).toHaveBeenCalled();
 });
 
 test('MapGL#onLoad', () => {
   const onLoad = jest.fn();
   mount(<MapGL latitude={0} longitude={0} zoom={0} onLoad={onLoad} />);
   expect(onLoad).toHaveBeenCalled();
+  // expect(onLoad).toHaveBeenCalledTimes(1);
 });
 
-// test('MapGL#onViewportChange', () => {
-//   const onViewportChange = jest.fn();
-//   mount(<MapGL
-//     latitude={0}
-//     longitude={0}
-//     zoom={0}
-//     onViewportChange={onViewportChange}
-//   />);
-//   expect(onViewportChange).toHaveBeenCalled();
-// });
+test('MapGL#viewport', () => {
+  const wrapper = mount(<MapGL latitude={0} longitude={0} zoom={0} />);
+
+  wrapper.setProps({ latitude: 1, longitude: 2, zoom: 3 });
+
+  expect(wrapper.props().latitude).toBe(1);
+  expect(wrapper.props().longitude).toBe(2);
+  expect(wrapper.props().zoom).toBe(3);
+});
