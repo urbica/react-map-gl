d84f26a4f76b1797f81b5d57ccaa6a3ee4426c02
Stepan Kuzmin <to.stepan.kuzmin@gmail.com>
2019-03-04 19:22:16 +0300

    auto commit [ci skip]

---
+++ precache-manifest.2e5cbf3243441579ff992c6f5856909c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "22759a0857b40b40c586",
+    "url": "build/main.22759a08.js"
+  },
+  {
+    "revision": "83e147d9301cac3803e5",
+    "url": "build/bundle.83e147d9.js"
+  },
+  {
+    "revision": "80e76e9717830b997db5",
+    "url": "static/css/2.183689b9.chunk.css"
+  },
+  {
+    "revision": "80e76e9717830b997db5",
+    "url": "build/2.80e76e97.js"
+  },
+  {
+    "revision": "8fd99de5ca4f21fc7b3fe1555f339697",
+    "url": "index.html"
+  }
+];
