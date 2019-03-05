fbc20c58ec94cea6daf587a97452b70d00528da1
Artem Boyur <gboyur@gmail.com>
2019-03-05 19:55:07 +0300

    fix(MapGL): fix nextLayerIds collection (#161)
    * fix(MapGL): fix nextLayerIds collection (@boyur)
    
    * test(MapGL): add multiple layers rendering test (@boyur)

--- src/components/MapGL/index.test.js
+++ src/components/MapGL/index.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { mount } from 'enzyme';
 
-import MapGL from '../..';
+import MapGL, { Layer, GeoJSONSource } from '../..';
 
 test('MapGL#render', () => {
   const wrapper = mount(<MapGL latitude={0} longitude={0} zoom={0} />);
@@ -29,4 +29,22 @@ test('MapGL#viewport', () => {
   expect(wrapper.props().latitude).toBe(1);
   expect(wrapper.props().longitude).toBe(2);
   expect(wrapper.props().zoom).toBe(3);
+});
+
+test('MapGL#multipleLayers', () => {
+  const data = { type: 'FeatureCollection', features: [] };
+
+  const wrapper = mount(
+    <MapGL latitude={0} longitude={0} zoom={0}>
+      <GeoJSONSource id="test" data={data} />
+      <Layer id="test1" type="circle" source="test" />
+      <Layer id="test2" type="circle" source="test" />
+    </MapGL>
+  );
+
+  expect(wrapper.find('Layer').exists()).toBe(true);
+  expect(wrapper.find('Layer')).toHaveLength(2);
+
+  wrapper.unmount();
+  expect(wrapper.find('Layer').exists()).toBe(false);
 });
