fce97f8bd7cdd5bc9c6e85aaa3f14588cedbcbe7
Stepan Kuzmin <to.stepan.kuzmin@gmail.com>
2019-03-12 19:26:01 +0300

    test(FeatureState): add test for FeatureState update

--- src/components/FeatureState/index.test.js
+++ src/components/FeatureState/index.test.js
@@ -17,3 +17,21 @@ test('FeatureState#render', () => {
   wrapper.unmount();
   expect(wrapper.find('FeatureState').exists()).toBe(false);
 });
+
+test('FeatureState#update', () => {
+  const data = { type: 'FeatureCollection', features: [] };
+
+  const wrapper = mount(
+    <MapGL latitude={0} longitude={0} zoom={0}>
+      <Source id="test" type="geojson" data={data} />
+      <FeatureState id="test" source="test" state={{ hover: false }} />
+    </MapGL>
+  );
+
+  wrapper.setProps({
+    children: [
+      <Source id="test" type="geojson" data={data} />,
+      <FeatureState id="test" source="test" state={{ hover: true }} />
+    ]
+  });
+});
